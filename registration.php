3b4161ae477919ae6c5c4ca190045ae659dd4f34
DBS-project1 <120077246+DBS-project1@users.noreply.github.com>
2022-12-08 11:32:18 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,4 +1,3 @@
-
 <html>
     
     <body align="center">
@@ -6,30 +5,38 @@
        <h3> DONOR REGISTRATION </h3><br>
         <form method="post">
         <label>Email id:</label>
-        <input type="text"  placeholder="enter email id" name="name" id="name"  required>
+        <input type="email"  placeholder="enter email id" name="name" id="name"  required>
         <br>
         <br>
         <label>Password:</label>
         <input type="password" placeholder="enter password" name="pass" id="password" required>
         <br><br>
-        <input type="submit" value="Submit"><br><br>
+        <input type="submit" value="Submit" name="submit"><br><br>
         </form>
 
 </div>
         </body>
         <?php
-        if(isset($_POST['name']) and isset($_POST['pass'])){
+        if(isset($_POST['name']) && isset($_POST['pass'])){
             $servername = "localhost";
             $username = "root";
             $password = "";
             $database="donor";
             
             $conn = mysqli_connect($servername, $username, $password,$database);
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($conn){
+            if (isset($_POST['submit'])) {
                 $name=$_POST['name'];
                 $password=$_POST['pass'];
-                $result="INSERT INTO DON_INFO VALUES('$name','$password')";
+                $result="INSERT INTO don_info(EMAIL_ID,PASSWORD1) VALUES('$name','$password')";
+                $q=mysqli_query($conn,$result);
+                echo $q;
               }
             }
+            else{
+                echo "no";
+            
+            }
+        }
         ?>
 </html>
